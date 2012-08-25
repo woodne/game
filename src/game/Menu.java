6bc2f32f6f60047f5f64a9905aa806dc3fd6328a
Josh Hartshorn <najenth@gmail.com>
2012-08-25 17:35:45 +0800

    freaking amazing menu

--- src/game/Menu.java
+++ src/game/Menu.java
@@ -8,28 +8,22 @@ import org.newdawn.slick.state.*;
 import org.lwjgl.input.Mouse;
 
 public class Menu extends BasicGameState{
-
+ //   Image play;
+ //   Image exit;
+    Image menu;
 
     public Menu(int state){
     }
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
-
+  //      play = new Image("res/play.png");
+  //      exit = new Image("res/exit.png");
+        menu = new Image("res/menu.png");
     }
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
-        g.drawString("This is the awesome menu!!", 200, 200);
-        g.fillRect(250,250,200, 50);
+        menu.draw();
     }
     public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
-        Input input = gc.getInput();
-        int xPos = Mouse.getX();
-        int yPos = Mouse.getY();
 
-
-        if((xPos > 250 && xPos < 450)&& (yPos>300 && yPos<350)){
-            if(input.isMouseButtonDown(0)){
-                sbg.enterState(1);
-            }
-        }
     }
     public int getID(){
         return 0;
--- src/game/display.java
+++ src/game/display.java
@@ -13,7 +13,7 @@ public class display extends BasicGameState{
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
     }
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
-        g.drawString("This is the game display!", 200, 200);
+
     }
     public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
     }
