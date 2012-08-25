d4a5bc07e0374c06fd15dee1cb43d070eda5a163
Josh Hartshorn <najenth@gmail.com>
2012-08-25 17:56:23 +0800

    modifying amazing menu to make it less of a joke

--- src/game/Menu.java
+++ src/game/Menu.java
@@ -8,19 +8,21 @@ import org.newdawn.slick.state.*;
 import org.lwjgl.input.Mouse;
 
 public class Menu extends BasicGameState{
- //   Image play;
- //   Image exit;
-    Image menu;
+    Image play;
+    Image exit;
+    int width = 800;
+    int height = 600;
+
 
     public Menu(int state){
     }
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
-  //      play = new Image("res/play.png");
-  //      exit = new Image("res/exit.png");
-        menu = new Image("res/menu.png");
+        play = new Image("res/play.png");
+        exit = new Image("res/exit.png");
     }
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
-        menu.draw();
+        play.draw(width - 700,height-100);
+        exit.draw(width - 200,height -100);
     }
     public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException{
 
